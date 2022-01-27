f1eba005556e6840f0bc50d16f3d88bfd99f6af6
anupam41 <anupamsamanta41@gmail.com>
2022-01-27 19:37:05 +0000

    sweet: switch to proton clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,8 @@ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := sweet_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
 
 # Partitions
